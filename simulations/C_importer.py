0623cef0568a1db762c6b38419ae09bea6f5173b
demKyr <64605968+demKyr@users.noreply.github.com>
2023-05-09 11:38:25 +0100

    Graph for approach C

---
+++ simulations/C_importer.py
@@ -0,0 +1,259 @@
+#%%
+
+from C_queueUsingDictionary import queue
+from datetime import datetime
+
+
+# Non-adjustable constants
+DPY = 0.000128 # 0.0128%
+stakeFee = 15 # MATIC
+unstakeFee = 15 # MATIC
+currentTimestamp = datetime.timestamp(datetime.now())
+
+expiryPeriod = 7 * 86400 # 7 days
+
+class batch:
+    def __init__(self):
+        self.balance = 0
+        self.expirydate = currentTimestamp + expiryPeriod
+        self.requests = []
+
+
+class ApproachC:
+    def __init__(self, fee = 0.5, depositFee = 0.001, withdrawFee = 0.001, expiryPeriodParam = 7 * 86400):
+        global expiryPeriod
+    
+        # Adjustable constants
+        self.fee = fee                # Fee on rewards
+        self.depositFee = depositFee
+        self.withdrawFee = withdrawFee
+        # batchSize = 15000 
+        self.batchSize = max(stakeFee / self.depositFee, unstakeFee / self.withdrawFee)
+
+        # Simulation parameters
+        self.Dbatches = queue()
+        self.Dbatches.enqueue(batch())
+        self.Wbatches = queue()
+        self.Wbatches.enqueue(batch())
+        self.shares = {}
+        self.preshares = {}
+        self.unshares = {}
+        self.VaultBalances = {"ValidatorAmount": 0, 
+                        "VaultAmount": 0,
+                        "rewards": 0,
+                        "claimedRewards":0,
+                        "TotalShares":0
+                        }
+
+
+    def XPY (self, days):
+        return (1+DPY)**days - 1
+
+
+    def sharePx(self):
+        if(self.VaultBalances['TotalShares'] == 0):
+            return 1
+        return (self.VaultBalances["ValidatorAmount"] + (self.VaultBalances['claimedRewards'] + self.VaultBalances["rewards"]) * (1-self.fee)) / self.VaultBalances["TotalShares"]
+
+
+    def time_pass(self, days, verbose = False):
+        global currentTimestamp
+        currentTimestamp += days * 86400
+        if(verbose):
+            print(days,'days pass by...(',datetime.fromtimestamp(currentTimestamp),')')
+        self.VaultBalances['rewards'] += self.VaultBalances['ValidatorAmount'] * self.XPY(days)
+        return
+
+
+    def show_state(self):
+        print('┌----------------------------------------------------------------------------┐')
+        print('|ValidatorAmount =', self.VaultBalances["ValidatorAmount"])
+        print('|VaultAmount =', self.VaultBalances["VaultAmount"])
+        print('|Rewards =', self.VaultBalances["rewards"])
+        print('|Claimed rewards =', self.VaultBalances["claimedRewards"])
+        print('|Share price =',self.sharePx())
+        print('|PreShares:',self.preshares)
+        print('|UnShares:',self.unshares)
+        print('|Total shares:',self.VaultBalances["TotalShares"])
+        print('|Shares:',self.shares)
+        print('|Balances from shares',{k:round(v*self.sharePx(),2) for k, v in self.shares.items()})
+        # print('|Deposit queue:',Dbatches.Q[Dbatches.last].requests)
+        # print('|Withdraw queue:',Wbatches.Q[Wbatches.last].requests)
+        print('|Deposit queue:', *((self.Dbatches.Q[idx].requests) for idx in self.Dbatches.Q), sep='\n                ')
+        print('|Withdraw queue:', *((self.Wbatches.Q[idx].requests) for idx in self.Wbatches.Q), sep='\n                ')
+        print('└----------------------------------------------------------------------------┘')
+        return
+
+
+    def claimRewards(self):
+        self.VaultBalances['claimedRewards'] += self.VaultBalances['rewards']
+        self.VaultBalances['rewards'] = 0
+        return
+
+
+
+    def stake(self, Dbatch):
+        self.SharePx = self.sharePx()
+        print('Staking',Dbatch.balance,'...')
+        for (user, amount) in Dbatch.requests:
+            self.preshares[user] -= amount
+            numOfShares = amount / self.SharePx
+            if user in self.shares:
+                self.shares[user] += numOfShares
+            else:
+                self.shares[user] = numOfShares
+            self.VaultBalances['TotalShares'] += numOfShares
+            
+        # mint shares from claimed rewards
+        numOfShares = self.VaultBalances['claimedRewards'] * self.fee / self.SharePx
+        self.shares['Treasury'] += numOfShares
+        self.VaultBalances['TotalShares'] += numOfShares
+        # stake amount by pay stake fee using VaultAmount and restake claimed rewards
+        self.VaultBalances["VaultAmount"] -= stakeFee
+        self.VaultBalances["ValidatorAmount"] += (Dbatch.balance + self.VaultBalances['claimedRewards'])
+        self.VaultBalances["VaultAmount"] -= Dbatch.balance 
+        self.VaultBalances['claimedRewards'] = 0
+        return
+
+
+    def unstake(self, Wbatch):
+        self.SharePx = self.sharePx()
+        print('Unstaking',Wbatch.balance,'...')
+        # unstake amount and pay unstake fee using VaultAmount
+        self.VaultBalances["VaultAmount"] -= unstakeFee
+        self.VaultBalances["ValidatorAmount"] -= Wbatch.balance
+        self.VaultBalances["VaultAmount"] += Wbatch.balance
+        for (user, amount) in Wbatch.requests:
+            numOfShares = amount / self.SharePx
+            self.shares[user] -= numOfShares
+            self.VaultBalances["VaultAmount"] -= amount
+            self.VaultBalances['TotalShares'] -= numOfShares
+            self.unshares[user] -= amount
+            # transfer amount to user
+        return
+
+
+    def expiryCheck(self):
+        if (not self.Dbatches.isEmpty() and self.Dbatches.Q[self.Dbatches.first].expirydate < currentTimestamp):
+            print('Deposit expired batch')
+            while(not self.Dbatches.isEmpty()):
+                self.stake(self.Dbatches.Q[self.Dbatches.first])
+                self.Dbatches.dequeue()
+            self.expiryCheck()
+        if (not self.Wbatches.isEmpty() and self.Wbatches.Q[self.Wbatches.first].expirydate < currentTimestamp):
+            print('Withdraw expired batch')
+            while(not self.Wbatches.isEmpty()):
+                self.unstake(self.Wbatches.Q[self.Wbatches.first])
+                self.Wbatches.dequeue()
+            self.expiryCheck()
+
+
+    def pairBatches(self, Dbatch, Wbatch):
+        self.SharePx = self.sharePx()
+        for (user, amount) in Wbatch.requests:
+            numOfShares = amount / self.SharePx
+            self.shares[user] -= numOfShares
+            self.unshares[user] -= amount
+            # tranfer amount to user but keep unstake fee 0.1% of amount
+            self.VaultBalances["VaultAmount"] -= amount * (1 - self.withdrawFee)
+        for (user, amount) in Dbatch.requests:
+            self.preshares[user] -= amount
+            numOfShares = amount / self.SharePx
+            if user in self.shares:
+                self.shares[user] += numOfShares
+            else:
+                self.shares[user] = numOfShares
+        return
+
+
+    def batchComplete(self):
+        if (self.Wbatches.isEmpty() or self.Dbatches.isEmpty()):
+            return
+        self.pairBatches(self.Dbatches.Q[self.Dbatches.first], self.Wbatches.Q[self.Wbatches.first])
+        self.Dbatches.dequeue()
+        self.Wbatches.dequeue()
+        return
+
+
+    def deposit(self, user, amount):
+        self.claimRewards()
+        # Add deposit fee 0.1% of amount
+        self.VaultBalances["VaultAmount"] += amount * (1 + self.depositFee)
+        # give preshares to user
+        if user in self.preshares:
+            self.preshares[user] += amount
+        else:
+            self.preshares[user] = amount
+        # add user's deposit request
+        if(amount + self.Dbatches.Q[self.Dbatches.last].balance <= self.batchSize):
+            self.Dbatches.Q[self.Dbatches.last].requests.append([user, amount])
+            self.Dbatches.Q[self.Dbatches.last].balance += amount
+        else:
+            freeSpace = self.batchSize - self.Dbatches.Q[self.Dbatches.last].balance
+            self.Dbatches.Q[self.Dbatches.last].requests.append([user, freeSpace])
+            self.Dbatches.Q[self.Dbatches.last].balance += freeSpace
+            self.batchComplete()
+            self.Dbatches.enqueue(batch())
+            self.Dbatches.Q[self.Dbatches.last].requests.append([user, amount - freeSpace])
+            self.Dbatches.Q[self.Dbatches.last].balance += amount - freeSpace
+        self.expiryCheck()
+        return
+
+
+    def withdraw(self, user, amount):
+        self.claimRewards()
+        if(self.preshares[user]>0):
+            print(user,'you cannot withdraw while having pending deposit requests')
+            return
+        if user in self.unshares:
+            if(self.shares[user] * self.sharePx() < amount + self.unshares[user]):
+                print(user,'you cannot withdraw more than you have staked')
+                return
+        else:
+            if(self.shares[user] * self.sharePx() < amount):
+                print(user,'you cannot withdraw more than you have staked')
+                return
+            
+
+        if user in self.unshares:
+            self.unshares[user] += amount
+        else:
+            self.unshares[user] = amount
+
+        if(amount + self.Wbatches.Q[self.Wbatches.last].balance <= self.batchSize):
+            self.Wbatches.Q[self.Wbatches.last].requests.append([user, amount])
+            self.Wbatches.Q[self.Wbatches.last].balance += amount
+        else:
+            freeSpace = self.batchSize - self.Wbatches.Q[self.Wbatches.last].balance
+            self.Wbatches.Q[self.Wbatches.last].requests.append([user, freeSpace])
+            self.Wbatches.Q[self.Wbatches.last].balance += freeSpace
+            self.batchComplete()
+            self.Wbatches.enqueue(batch())
+            self.Wbatches.Q[self.Wbatches.last].requests.append([user, amount - freeSpace])
+            self.Wbatches.Q[self.Wbatches.last].balance += amount - freeSpace
+        self.expiryCheck()
+        return
+
+    def initialisation(self):
+        global currentTimestamp
+        currentTimestamp = datetime.timestamp(datetime.now())
+        self.Dbatches = queue()
+        self.Dbatches.enqueue(batch())
+        self.Wbatches = queue()
+        self.Wbatches.enqueue(batch())
+        self.shares = {}
+        self.preshares = {}
+        self.unshares = {}
+        self.VaultBalances = {"ValidatorAmount": 0, 
+                        "VaultAmount": 0,
+                        "rewards": 0,
+                        "claimedRewards":0,
+                        "TotalShares":0
+                        }
+        print('fee',self.fee)
+        print('depositFee',self.depositFee)
+        print('withdrawFee',self.withdrawFee)
+        print('batchSize',self.batchSize)
+        print('timestamp',datetime.fromtimestamp(currentTimestamp))
+        self.shares['Treasury'] = 0
+    
---
+++ simulations/C_profit.py
@@ -0,0 +1,134 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider, Button
+from C_importer import ApproachC
+
+
+# Non-adjustable constants
+DPY = 0.000128 # 0.0128%
+stakeFee = 15 # MATIC
+unstakeFee = 15 # MATIC
+num_of_days = 365
+
+# Simulation parameters
+initialVaultInjection = 1000
+feeInit=0.1
+depositFee=0.001
+withdrawFee=0.001
+expiryPeriodParam = 7 * 86400
+initialAmountInValidatorInit = 10000
+dailyDepositsInit = 1500
+dailyWithdrawalsInit = 1000
+
+t = np.linspace(0, num_of_days, num_of_days)
+
+def XPY (days):
+    return (1+DPY)**days - 1
+
+def approachCsim(fee, dailyDeposits, dailyWithdrawals):
+    AC = ApproachC(fee=fee, depositFee=depositFee, withdrawFee=withdrawFee, expiryPeriodParam = expiryPeriodParam)
+    AC.initialisation()
+    AC.show_state()
+    profitsFromRewards = []
+    for day in range(num_of_days):
+        AC.time_pass(1)
+        AC.deposit('user', dailyDeposits)
+        AC.withdraw('user', dailyWithdrawals)
+        profitsFromRewards.append(AC.shares['Treasury'] * AC.sharePx() + (AC.VaultBalances['claimedRewards'] + AC.VaultBalances['rewards']) * fee)
+    return profitsFromRewards
+
+
+# Create the figure and the line that we will manipulate
+fig, ax = plt.subplots()
+profitsFromRewards = approachCsim(initialAmountInValidatorInit, dailyDepositsInit, dailyWithdrawalsInit)
+line1, = ax.plot(t, profitsFromRewards, lw=2, label = "Profit from rewards")
+ax.set_ylabel('Profit [MATIC]')
+ax.set_xlabel('Deposit [MATIC]')
+# ax.set_ylim(-40, 1500)
+ax.axhline(y = 0, color = 'y', linestyle = '--')
+
+# adjust the main plot to make room for the sliders
+fig.subplots_adjust(bottom=0.4)
+
+
+# Make a horizontal slider to control fee
+axFee = fig.add_axes([0.25, 0.1, 0.65, 0.03])
+fee_slider = Slider(
+    ax=axFee,
+    label='Fee',
+    valmin=0,
+    valmax=1,
+    valinit=feeInit,
+    valstep=0.05,
+)
+
+# Make a horizontal slider to control dailyDeposits
+axDailyDeposits = fig.add_axes([0.25, 0.15, 0.65, 0.03])
+dailyDeposits_slider = Slider(
+    ax=axDailyDeposits,
+    label='dailyDeposits',
+    valmin=0,
+    valmax=30000,
+    valinit=dailyDepositsInit,
+    valstep=500,
+)
+
+# Make a horizontal slider to control dailyWithdrawals
+axDailyWithdrawals = fig.add_axes([0.25, 0.2, 0.65, 0.03])
+dailyWithdrawals_slider = Slider(
+    ax=axDailyWithdrawals,
+    label='dailyWithdrawals',
+    valmin=0,
+    valmax=30000,
+    valinit=dailyWithdrawalsInit,
+    valstep=500,
+)
+
+
+
+
+
+
+# The function to be called anytime a slider's value changes
+def update(val):
+
+    profitsFromRewards = approachCsim(fee_slider.val, dailyDeposits_slider.val, dailyWithdrawals_slider.val)
+
+    ax.set_ylim(-10, 1.05 * max(profitsFromRewards))
+    line1.set_ydata(profitsFromRewards)
+    
+    fig.canvas.draw_idle()
+
+
+# register the update function with each slider
+fee_slider.on_changed(update)
+dailyDeposits_slider.on_changed(update)
+dailyWithdrawals_slider.on_changed(update)
+
+# Create a `matplotlib.widgets.Button` to reset the sliders to initial values.
+resetax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
+button = Button(resetax, 'Reset', hovercolor='0.975')
+
+
+
+textstr = '\n'.join((
+    r'DPY=$%.4f$%%' % (DPY*100, ),
+    r'MPY(30days)=$%.4f$%%' % (XPY(30)*100, ),
+    r'Stake fee=$%.2f$ MATIC' % (stakeFee, ),
+    r'Unstake fee=$%.2f$ MATIC' % (unstakeFee, )))
+
+props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+
+# place a text box in upper left in axes coords
+ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14,
+        verticalalignment='top', bbox=props)
+
+
+def reset(event):
+    fee_slider.reset()
+    dailyDeposits_slider.reset()
+    dailyWithdrawals_slider.reset()
+button.on_clicked(reset)
+
+ax.legend(loc='upper right')
+plt.show()
