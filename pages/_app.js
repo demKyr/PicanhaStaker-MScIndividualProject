a17fd4d7860f80d86dc0ba1153ccdd54e506f9c9
demKyr <64605968+demKyr@users.noreply.github.com>
2023-08-16 17:56:00 +0100

    Refresh site on account change, Ask user to switch to Goerli

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import { Web3ReactProvider } from "@web3-react/core";
 import { Web3Provider } from "@ethersproject/providers";
+import { useEffect, useState } from "react";
 
 import Layout from "../components/layout/layout";
 import { ContractsContextProvider } from "../store/contract-context";
@@ -24,13 +25,58 @@ const getLibrary = (provider) => {
 
 
 function MyApp({ Component, pageProps }) {
+  const [networkError, setNetworkError] = useState(false);
+
+  useEffect(() => {
+    const handleAccountsChanged = (accounts) => {
+      window.location.reload();
+    };
+
+    const checkNetwork = async () => {
+      try {
+        const network = await window.ethereum.request({
+          method: "net_version",
+        });
+
+        if (network !== "5") { // Goerli network ID is "5"
+          setNetworkError(true);
+        } else {
+          setNetworkError(false);
+        }
+      } catch (error) {
+        console.error("Error checking network:", error);
+      }
+    };
+
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", handleAccountsChanged);
+      checkNetwork();
+
+      window.ethereum.on("chainChanged", () => {
+        checkNetwork();
+      });
+    }
+
+    return () => {
+      if (window.ethereum) {
+        window.ethereum.removeListener("accountsChanged", handleAccountsChanged);
+      }
+    };
+  }, []);
+
   return (
     <ContractsContextProvider>
-      <Web3ReactProvider getLibrary={getLibrary}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </Web3ReactProvider>
+        {networkError ? (
+          <div className="error-message">
+            <p>Please switch your wallet to the Goerli test network.</p>
+          </div>
+        ) : (
+          <Web3ReactProvider getLibrary={getLibrary}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </Web3ReactProvider>
+        )}
     </ContractsContextProvider>
   );
 }
