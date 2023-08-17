de598342040f650ddce1a24189ab532156ef86fb
demKyr <64605968+demKyr@users.noreply.github.com>
2023-08-17 23:16:58 +0100

    Add noopener noreferrer

--- Frontend/components/layout/footer.js
+++ Frontend/components/layout/footer.js
@@ -5,10 +5,10 @@ function Footer() {
     <footer className={classes.footer}>
 
       <div className={classes.footerContent}>
-          <a href="https://goerli.etherscan.io/address/0x9437eff6e8713cf1619d9507695489a6639b758d#code" target="_blank" className={classes.link}>View smart contract here</a>
+          <a href="https://goerli.etherscan.io/address/0x9437eff6e8713cf1619d9507695489a6639b758d#code" target="_blank" rel="noopener noreferrer" className={classes.link}>View smart contract here</a>
         <p className={classes.footerNote}>
           TruStake Picanha vault is designed and developed by Demetris Kyriacou as part of an MSc Individual Project for Imperial College London.
-          It constitutes an extended version of <span><a href="https://app.trufin.io/vaults/trustake" target="_blank">TruStake vault</a></span> by <span><a href="https://www.trufin.io/" target="_blank">trufin.io&trade;</a></span>
+          It constitutes an extended version of <span><a href="https://app.trufin.io/vaults/trustake" target="_blank" rel="noopener noreferrer">TruStake vault</a></span> by <span><a href="https://www.trufin.io/" target="_blank" rel="noopener noreferrer">trufin.io&trade;</a></span>
         </p>
       </div>
     </footer>
